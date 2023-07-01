607f2269089be00f41fdc28a3832cd66683dbe69
David Nabergoj <davidnabergoj4@gmail.com>
2023-07-01 03:14:45 +0200

    Add tests for GaussianMixture2D

--- potentials/test/test_basic.py
+++ potentials/test/test_basic.py
@@ -3,6 +3,7 @@ import torch
 
 from potentials.base import PotentialSimple
 from potentials.synthetic.gaussian.full_rank import IllConditionedGaussian
+from potentials.synthetic.gaussian_mixture import GaussianMixture2D
 
 from potentials.synthetic.funnel import Funnel
 
@@ -13,7 +14,7 @@ from potentials.synthetic.funnel import Funnel
 def test_evaluation(batch_shape, n_dim, potential_class: PotentialSimple):
     torch.manual_seed(0)
     potential = potential_class(n_dim=n_dim)
-    x = torch.randn(size=(*batch_shape, potential.n_dim))
+    x = torch.randn(size=(*batch_shape, *potential.event_shape))
     u = potential(x)
     assert u.shape == batch_shape
     assert torch.all(~torch.isnan(u))
@@ -27,6 +28,29 @@ def test_sampling(batch_shape, n_dim, potential_class: PotentialSimple):
     torch.manual_seed(0)
     potential = potential_class(n_dim=n_dim)
     x = potential.sample(batch_shape)
-    assert x.shape == (*batch_shape, n_dim)
+    assert x.shape == (*batch_shape, *potential.event_shape)
+    assert torch.all(~torch.isnan(x))
+    assert torch.all(~torch.isinf(x))
+
+
+@pytest.mark.parametrize('potential_class', [GaussianMixture2D])
+@pytest.mark.parametrize('batch_shape', [(2, 3), (5, 4), (7, 19, 2), (11,)])
+def test_fixed_n_dim_evaluation(batch_shape, potential_class):
+    torch.manual_seed(0)
+    potential = potential_class()
+    x = torch.randn(size=(*batch_shape, *potential.event_shape))
+    u = potential(x)
+    assert u.shape == batch_shape
+    assert torch.all(~torch.isnan(u))
+    assert torch.all(~torch.isinf(u))
+
+
+@pytest.mark.parametrize('potential_class', [GaussianMixture2D])
+@pytest.mark.parametrize('batch_shape', [(2, 3), (5, 4), (7, 19, 2), (11,)])
+def test_fixed_n_dim_sampling(batch_shape, potential_class):
+    torch.manual_seed(0)
+    potential = potential_class()
+    x = potential.sample(batch_shape)
+    assert x.shape == (*batch_shape, *potential.event_shape)
     assert torch.all(~torch.isnan(x))
     assert torch.all(~torch.isinf(x))
