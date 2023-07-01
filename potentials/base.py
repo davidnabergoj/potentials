6ae17160c0a2558e59770b5c8751b40a4fc8199a
David Nabergoj <davidnabergoj4@gmail.com>
2023-07-01 13:41:02 +0200

    Update typehints

--- potentials/base.py
+++ potentials/base.py
@@ -4,7 +4,7 @@ import torch
 
 
 class Potential:
-    def __init__(self, event_shape: Union[torch.Size, Tuple[int]]):
+    def __init__(self, event_shape: Union[torch.Size, Tuple[int, ...]]):
         self.event_shape = event_shape
 
     def compute(self, x: torch.Tensor) -> torch.Tensor:
@@ -13,7 +13,7 @@ class Potential:
     def __call__(self, *args, **kwargs):
         return self.compute(*args, **kwargs)
 
-    def sample(self, batch_shape: Union[torch.Size, Tuple[int]]) -> torch.Tensor:
+    def sample(self, batch_shape: Union[torch.Size, Tuple[int, ...]]) -> torch.Tensor:
         raise NotImplementedError
 
 
