91b8428e91cbf4b2e1e9ffedb67f12ab33d0f4cf
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-11 10:24:23 +0200

    Add constant

--- potentials/synthetic/gaussian/unit.py
+++ potentials/synthetic/gaussian/unit.py
@@ -13,7 +13,7 @@ class StandardGaussian(Potential):
     def compute(self, x: torch.Tensor) -> torch.Tensor:
         batch_shape = get_batch_shape(x, self.event_shape)
         sum_dims = list(range(len(batch_shape), len(batch_shape) + len(self.event_shape)))
-        return torch.sum(x ** 2, dim=sum_dims)
+        return torch.sum(0.5 * (x ** 2), dim=sum_dims)
 
     def sample(self, batch_shape: Union[torch.Size, Tuple[int]]) -> torch.Tensor:
         return torch.randn(*batch_shape, *self.event_shape)
