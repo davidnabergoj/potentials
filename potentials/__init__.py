ea892d9607c1e4947747598d70355d6698ca571f
David Nabergoj <davidnabergoj4@gmail.com>
2023-08-31 14:37:35 +0200

    Add double Gaussian

--- potentials/__init__.py
+++ potentials/__init__.py
@@ -1,5 +1,5 @@
 from potentials.synthetic.funnel import Funnel
 from potentials.synthetic.gaussian.full_rank import IllConditionedGaussian
-from potentials.synthetic.gaussian_mixture import GaussianMixture2D
+from potentials.synthetic.gaussian_mixture import GaussianMixture2D, DoubleGaussian
 from potentials.synthetic.gaussian.unit import StandardGaussian
 from potentials.synthetic.gaussian.diagonal import DiagonalGaussian
