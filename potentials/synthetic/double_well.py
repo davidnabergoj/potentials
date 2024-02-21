24002d1040c1bd7f367bb049ee5495c95c9f24af
David Nabergoj <davidnabergoj4@gmail.com>
2024-02-21 08:37:52 +0100

    Add double well ground truth variance

--- potentials/synthetic/double_well.py
+++ potentials/synthetic/double_well.py
@@ -15,6 +15,17 @@ class DoubleWell(PotentialSimple):
     def mean(self) -> torch.Tensor:
         return torch.zeros(self.n_dim)
 
+    @property
+    def variance(self) -> torch.Tensor:
+        if self.distance == 4:
+            marginal_var = 3.9341046423344384502056756017641461798253050095405973357175507668
+            # Integrate[exp(-(x^2-4)^2) * x^2,{x,-∞,∞}] / Integrate[exp(-(y^2-4)^2),{y,-∞,∞}]
+        elif self.distance == 2:
+            marginal_var = 1.8353417214904593599217904198784863635172262491493630166971601894
+        else:
+            raise ValueError
+        return torch.full(size=(self.n_dim,), fill_value=marginal_var)
+
     def compute(self, x: torch.Tensor):
         y = (x ** 2 - self.distance) ** 2
         return sum_except_batch(y, batch_shape=get_batch_shape(y, self.event_shape))
