533e4626e6100ec52564a45aff4295b8a7d6901c
David Nabergoj <davidnabergoj4@gmail.com>
2024-08-20 22:06:10 +0200

    Add return type to sum_except_batch

--- potentials/utils.py
+++ potentials/utils.py
@@ -27,7 +27,7 @@ def unsqueeze_to_batch(x: torch.Tensor, batch_shape: torch.Size):
     return x[(None,) * len(batch_shape)]
 
 
-def sum_except_batch(x: torch.Tensor, batch_shape: torch.Size):
+def sum_except_batch(x: torch.Tensor, batch_shape: torch.Size) -> torch.Tensor:
     sum_dims = tuple(range(len(batch_shape), len(x.shape)))
     return torch.sum(x, dim=sum_dims)
 
