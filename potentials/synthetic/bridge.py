d6732d58c5249d775237115a6f45b84da0de008d
David Nabergoj <davidnabergoj4@gmail.com>
2024-10-11 11:40:39 +0200

    Remove attribute, change parameters

--- potentials/synthetic/bridge.py
+++ potentials/synthetic/bridge.py
@@ -19,11 +19,10 @@ class BrownianBridge(GaussianProcess):
     Reference: Brofos et al. "Adaptation of the Independent Metropolis-Hastings Sampler with Normalizing Flow Proposals"
      (2022). url: https://proceedings.mlr.press/v151/brofos22a.html.
     """
-    def __init__(self, n_points: int = 50, eps: float = 1e-3):
+    def __init__(self, n_points: int = 50, eps: float = 1e-2):
         t = torch.linspace(eps, 1 - eps, n_points)
         s = torch.clone(t)
         super().__init__(
             mu=torch.sin(torch.pi * t),
             cov=torch.minimum(t[:, None], s[None]) - s[None] * t[:, None]
         )
-        self.n_points = n_points
